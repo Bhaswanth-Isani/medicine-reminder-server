50769a560f81501a23a5d591d4a615f49ab4659a
Bhaswanth Isani <bhaswanthisani@gmail.com>
2023-01-14 15:06:21 +0530

    Added decreaseMedicine controller

--- src/controllers/medicineController.ts
+++ src/controllers/medicineController.ts
@@ -4,11 +4,12 @@ import { invalidInputDataError, serverError, unauthorizedError } from '../helper
 import { z } from 'zod'
 import { prisma } from '../../prisma'
 
-// @desc    Creates a new medicine for a particular user using name, compartment, number of pills and time   POST /user/create-account
+// @desc    Creates a new medicine for a particular user using name, compartment, number of pills and time
+// @route   POST /medicine/create-medicine
 // @access  Private
 // @input   {name: string, compartment: string, number: int, time: string[]}
-// @output  {id: string, name: string, compartment: string, number: int, time: string[], userID: string}
-export const createMedicine = async (req: TypedRequestBody<{ name: string, compartment: string, number: number, time: string[] }>, res: Express.Response): Promise<void> => {
+// @output  {id: string, name: string, compartment: number, number: int, time: string[], userID: string, success: boolean}
+export const createMedicine = async (req: TypedRequestBody<{ name: string, compartment: number, number: number, time: string[] }>, res: Express.Response): Promise<void> => {
   const { name, compartment, number, time } = req.body
   const userID = req.user?.id
 
@@ -38,7 +39,7 @@ export const createMedicine = async (req: TypedRequestBody<{ name: string, compa
               id: previousMedicineInCompartment.id
             },
             data: {
-              compartment: '0'
+              compartment: 0
             }
           })
         }
@@ -53,7 +54,7 @@ export const createMedicine = async (req: TypedRequestBody<{ name: string, compa
           }
         })
 
-        res.status(200).json({ ...medicine })
+        res.status(200).json({ ...medicine, success: true })
       }
     } catch (error) {
       res.status(500).json({
@@ -61,5 +62,48 @@ export const createMedicine = async (req: TypedRequestBody<{ name: string, compa
         error: serverError
       })
     }
+  }
+}
+
+// @desc    Decreases the amount of medicine by 1
+// @route   POST /medicine/decrease-medicine
+// @access  Private
+// @input   {userID: string, compartment: number}
+// @output  {success: boolean}
+export const decreaseMedicine = async (req: TypedRequestBody<{ userID: string, compartment: number }>, res: Express.Response): Promise<void> => {
+  const { userID, compartment } = req.body
+
+  try {
+    if (!z.string().min(10).safeParse(userID).success || !z.number().min(1).max(3).safeParse(compartment).success) {
+      res.status(401).json({
+        success: false,
+        error: unauthorizedError
+      })
+    } else {
+      const previousData = await prisma.medicine.findFirst({
+        where: {
+          userId: userID,
+          compartment
+        }
+      })
+
+      await prisma.medicine.update({
+        where: {
+          id: previousData.id
+        },
+        data: {
+          number: previousData.number - 1
+        }
+      })
+
+      res.status(200).json({
+        success: true
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: serverError
+    })
   }
 }
--- src/routes/medicineRoute.ts
+++ src/routes/medicineRoute.ts
@@ -1,7 +1,8 @@
 import express from 'express'
-import { createMedicine } from '../controllers/medicineController'
+import { createMedicine, decreaseMedicine } from '../controllers/medicineController'
 import { isLoggedIn } from '../middleware/isLoggedIn'
 
 export const medicineRouter = express.Router()
 
 medicineRouter.post('/create-medicine', isLoggedIn, createMedicine)
+medicineRouter.post('/decrease-medicine', isLoggedIn, decreaseMedicine)
